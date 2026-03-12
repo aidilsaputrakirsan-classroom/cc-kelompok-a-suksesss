faf19abfe9055e9123bafcdbc0e7d69d10486b54
Liveral <rizkiaa1502@gmail.com>
2026-03-12 13:10:54 +0800

    update .gitinore & setup .env dan .env.example untuk frontend

--- setup.sh
+++ setup.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-echo "Memulai instalasi dependencies backend..."
-
-# Berpindah ke folder backend
+echo "=== Memulai instalasi dependencies Backend ==="
 cd backend || exit
-
-# Menginstal library dari requirements.txt
 pip install -r requirements.txt
+echo "Backend selesai!"
 
-echo "Instalasi selesai!"
+# Kembali ke folder utama (root) sebelum masuk ke frontend
+cd .. 
+
+echo "=== Memulai instalasi dependencies Frontend ==="
+cd frontend || exit
+npm install 
+echo "Frontend selesai!"
+
+echo "=== Semua instalasi telah selesai! Aplikasi siap dijalankan. ==="
